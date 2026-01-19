8e34109275f807322ae290ff7e0058f489c3ebd3
Andika Tirta (mobile) <andikatirta001@gmail.com>
2026-01-19 11:54:55 +0700

    Fix icon SEO setup & Update Dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,10 +15,6 @@ export const metadata: Metadata = {
   },
   description:
     "PT Rimbun Daur Alam mengembangkan teknologi ramah lingkungan untuk pengolahan sampah dan energi terbarukan, termasuk sistem RDF briket Aureloop.",
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon.ico",
-  },
   openGraph: {
     type: "website",
     siteName: "PT Rimbun Daur Alam",
@@ -52,6 +48,24 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
