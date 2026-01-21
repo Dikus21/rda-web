7ecec22d191684345eb6290f3fc137049fc78a50
Andika Tirta (mobile) <andikatirta001@gmail.com>
2026-01-21 12:01:14 +0700

    Put other icon size on hold

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.ico" />
-        <link
+        {/* <link
           rel="apple-touch-icon"
           sizes="180x180"
           href="/apple-touch-icon.png"
@@ -65,7 +65,7 @@ export default function RootLayout({
           sizes="16x16"
           href="/favicon-16x16.png"
         />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/site.webmanifest" /> */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
